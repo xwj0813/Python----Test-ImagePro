5af9cb5589abf3933ada7b189b7385845c76e4ff
xwj0813 <32487585+xwj0813@users.noreply.github.com>
2017-10-30 17:51:42 +0800

    Create dectectTXZ.py
    创造一个class：TextDectTXZ，里面定义切割通行证文字所使用的函数

---
+++ dectectTXZ.py
@@ -0,0 +1,99 @@
+'文字定位，定位身份证中的文字区域块'"
+import  cv2 ,numpy as np
+
+#文字定位器类
+class TextDectTXZ (object):
+    def __init__(self,imgpath):
+        self.img=cv2.imread(imgpath)
+       # cv2.imshow('d',self.img)
+        #cv2.waitKey(0)
+        self.resize_img=self.transform()
+    def transform(self):
+        #归一化尺寸
+        h,w,c=np.shape(self.img)
+        f_h=1000
+        f_y=float(h)/1000
+        f_w=int(float(w)/f_y)
+        resize_img=cv2.resize(self.img,(f_w,f_h),interpolation=cv2.INTER_AREA)
+        return resize_img
+    def hyp_paramters(self):
+        #计算经验参数
+        h,w,c=np.shape(self.resize_img)
+        #文字区域最小最大面积
+        min_area,max_area=25*25,0.04*h*w
+        #文字区块最小最大高度
+        min_h,max_h=30,100
+        return min_area,max_area,min_h,max_h
+    def find_candidates(self):
+        ##返回候选区文字区块坐标
+        #灰度化
+        gray=cv2.cvtColor(self.resize_img,cv2.COLOR_BGR2GRAY)
+        #二值化
+        _threshold=cv2.threshold(gray,115,255,cv2.THRESH_BINARY)[1]
+        #腐蚀
+        kernel=np.ones((2,4),np.uint8)
+        erosion=cv2.erode(_threshold,kernel,iterations=2)
+        cv2.imwrite("F:/image/Word/thresh.jpg",_threshold)
+        #去噪
+
+        #轮廓检测
+        _,contours,hierarchy=cv2.findContours(erosion,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        #根据经验参数获取区域
+        candidate_boxes=[]
+        min_area,max_area,min_h,max_h=self.hyp_paramters()
+        for cnt in contours:
+            x,y,w,h=cv2.boundingRect(cnt)
+            if min_area<cv2.contourArea(cnt)<max_area and min_h<h<max_h:
+                candidate_boxes.append((x,y,x+w,y+h))
+        return candidate_boxes
+    def split_candidates(self,box):
+        #候选区块切割
+        gray=cv2.cvtColor(self.resize_img,cv2.COLOR_BGR2GRAY)
+        #二值化
+        _threshold = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY)[1]
+        # 腐蚀
+        kernel = np.ones((2, 4), np.uint8)
+        erosion = cv2.erode(_threshold, kernel, iterations=2)
+        # 获取区块对应腐蚀图像
+        x1, y1, x2, y2 = box
+        box_erosion = erosion[y1:y2, x1:x2]
+        # 依据连续空白区域进行列切割
+        his = np.mean(box_erosion, axis=0)
+        split_indexes, flag = [0], True
+        for i in range(len(his)):
+            c = his[i]
+            if c != 255 and flag is True:
+                split_indexes.append(i)
+                flag = False
+            elif c == 255 and flag is False:
+                split_indexes.append(i)
+                flag = True
+        split_indexes.append(len(his))
+        #assert len(split_indexes) % 2 == 0
+        t = [int((i + j) / 2) for i, j in zip(split_indexes[::2], split_indexes[1::2])]
+        boxes = [(x1 + t[i], y1, x1 + t[i + 1], y2) for i in range(len(t) - 1) if t[i + 1] - t[i]]
+        return boxes
+    def is_noisy(self,box):
+        #基于坐标规则判断是否属于人脸噪声区块
+        h, w, c = np.shape(self.resize_img)
+        x1, y1, x2, y2 = box
+        if 0.15*h<(y1 + y2) / 2 and (x1 + x2) / 2<0.28 * w and (y1 + y2) / 2 < 0.8* h :
+            return True
+        return False
+
+    def detect(self):
+        # 初步查找候选区域
+        candidate_boxes = self.find_candidates()
+        # 初步候选区域切割
+        split_boxes = []
+        for candidate in candidate_boxes:
+            if candidate[2] - candidate[0] > 100:
+                split_boxes += self.split_candidates(candidate)
+            else:
+                split_boxes += [candidate]
+        # 屏蔽人脸区域噪音
+        final_boxes = []
+        for box in split_boxes:
+            if not self.is_noisy(box):
+                final_boxes.append(box)
+        return final_boxes
